fc9343efda190a13b54761a2d392e88cb709801a
Werner1020p <149018990+Werner1020p@users.noreply.github.com>
2026-05-19 21:28:50 -0400

    "added front-end"

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Public Archive' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Messages in a Bottle',
+    subtitle: 'Leave an anonymous message behind for someone else to discover.',
+    introText: 'This public archive collects messages, confessions, hopes, and thoughts from strangers. Drop your message into the sea and explore what others left behind.'
+  });
 });
 
 module.exports = router;
